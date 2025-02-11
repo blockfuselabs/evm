ff87366050971bc56538a83eaf5fcdab8db102c7
mananuf <bmananaf@gmail.com>
2025-02-11 23:30:17 +0100

    adds stack base implementation

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,6 +1,22 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+
+	"github.com/blockfuselabs/evm/types"
+)
 
 func main(){
-fmt.Println("I am invinsible")
+	s := types.NewStack()
+	// s.Data = append(s.Data, types.Byte32{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1})
+	// s.Data = append(s.Data, types.Byte32{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1})
+
+	// for i := 0; i < 1024; i++  {
+	// 	s.Push(types.Uint256{1})
+	// }
+	s.Push(types.Uint256{1})
+	s.Push(types.Uint256{1})
+	s.Push(types.Uint256{1,1,1,1})
+	s.Pop()
+	fmt.Println(s.Data)
 }
